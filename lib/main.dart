59313be7976d3b7468d802e3d87b8bf9b87fcb3d
Stefanny_Freitas <stefanny.freitas10@gmail.com>
2024-11-06 23:43:30 +0000

    Alter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import './view/dashboard_screen.dart';
+import './view/exercise_screen.dart';
+import './view/health_mate_app.dart';
+import './view/login_screen.dart';
+import './view/meal_screen.dart';
+import './view/registration_screen.dart';
+import './view/sleep_screen.dart';
 
 void main() {
   runApp(HealthMateApp());
